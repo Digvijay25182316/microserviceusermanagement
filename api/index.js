30297393feb7d4bd6aea9cf188921c14ed9cfcef
Digvijay25182316 <digvijays.edake2002@gmail.com>
2023-12-02 21:38:01 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const DB = require("../config/DB");
 const a = require("../app");
+const u = require("./routes/route");
+a.use("/api", u);
 //connecting the database
 DB();
 const P = process.env.PORT || 4053;
--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const e = require("express");
 const a = e();
-const u = require("./routes/route");
 //body parsing middleware
 a.use(e.json());
-a.use("/", u);
 module.exports = a;
